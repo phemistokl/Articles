d859207a3869a370f5218be14f159b2fd2175c98
Albert Wood <phemistokl@gmail.com>
2016-08-07 22:35:53 +0300

    first step Articles

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = React.createClass({
+      render: function() {
+        return (
+          <div className="app">
+            App
+          </div>
+        );
+      }
+});
+
+export default App;
